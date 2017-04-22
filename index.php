3e181a875b9c9dd965df14ab2662dd6e1efba68b
jordanbrauer <jbrauer.inc@gmail.com>
2017-04-22 16:08:06 -0500

    WIP: Working example of the router class

--- index.php
+++ index.php
@@ -5,3 +5,11 @@ require_once 'vendor/autoload.php';
 use \Jorb\Router\Router as Router;
 
 $router = new Router();
+
+$router
+  ->add('/')
+  ->add('/about')
+  ->add('/contact')
+;
+
+$router->dispatch();
