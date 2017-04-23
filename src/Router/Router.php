5e8285fd819773c0d4a65b272c802da5471212c8
jordanbrauer <jbrauer.inc@gmail.com>
2017-04-22 20:09:26 -0500

    Feat: moved match() method into dispatch() and added methods property

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -5,38 +5,26 @@ namespace Jorb\Router;
 class Router
 {
   private $uri;
+  private $methods;
 
   public function __construct ()
   {
     $this->uri = array();
+    $this->methods = array();
   }
 
   /**
    * Add a URI to internal $uri array property
    * @param string $uri The URI to be added
+   * @param mixed $method The method associated with a new URI
+   * @param object
    */
-  public function add ($uri)
+  public function add ($uri, $method)
   {
     array_push($this->uri, $uri);
+    array_push($this->methods, $method);
+
     return $this;
-  }
-
-  /**
-   * Look for a matching route to a request
-   * @param string $request The url being searched for
-   * @return boolean
-   */
-  public function match ($request)
-  {
-    $routes = $this->uri;
-
-    foreach ($routes as $id => $route) {
-      if (preg_match("#^{$route}$#", $request)) {
-        return true;
-      }
-    }
-
-    return false;
   }
 
   /**
@@ -45,11 +33,29 @@ class Router
   public function dispatch ()
   {
     $request = isset($_GET['uri']) ? rtrim("/{$_GET['uri']}", '/') : '/';
+    $args = array();
 
-    if (!Self::match($request, $this->uri)) {
-      echo "No match found for: <code>{$request}</code><br>";
-    } else {
-      echo "Found match for: <code>$request</code><br>";
+    // BUG: error thrown for every non-matching route
+    foreach ($this->uri as $id => $route) {
+      if (preg_match("#^{$route}$#", $request)) {
+        $uri = explode('/', $request);
+        $mock = explode('/', $route);
+
+        echo '<pre>';
+        print_r($uri);
+        print_r($mock);
+        echo '</pre>';
+
+        foreach ($mock as $key => $value) {
+          if ($value == '.+') {
+            array_push($args, $uri[$key]);
+          }
+        }
+
+        call_user_func_array($this->methods[$id], $args);
+      } else {
+        echo "No route for: <code>{$request}</code><br>";
+      }
     }
   }
 }
