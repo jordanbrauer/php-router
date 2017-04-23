1504c29923bf7937fb44d514d0a557cca03ffd25
jordanbrauer <jbrauer.inc@gmail.com>
2017-04-22 22:53:12 -0500

    Feat: usage examples in main index file

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once 'vendor/autoload.php';
 
 use \Jorb\Router\Router as Router;
@@ -7,9 +10,25 @@ use \Jorb\Router\Router as Router;
 $router = new Router();
 
 $router
-  ->add('/')
-  ->add('/about')
-  ->add('/contact')
+  ->add('/', function() {
+    echo "<h3>Index</h3>";
+  })
+
+  ->add('/about', function() {
+    echo "<h3>About</h3>";
+  })
+
+  ->add('/contact', function() {
+    echo "<h3>Contact</h3>";
+  })
+
+  ->add('/user/.+', function($user) {
+    echo "<h3>User</h3><p>{$user}</p>";
+  })
+
+  ->add('/user/.+/repository/.+', function($user, $repository) {
+    echo "<h3>Repository</h3><p>{$user}/{$repository}</p>";
+  })
 ;
 
 $router->dispatch();
